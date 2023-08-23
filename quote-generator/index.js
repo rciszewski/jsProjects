eebf475242592b43fb557e4a26ea4ed268665241
Ron <rc72095@icloud.com>
2023-08-23 11:38:48 -0400

    add ability to tweet quote and fetch new quote with twitter and new quote buttons

--- quote-generator/index.js
+++ quote-generator/index.js
@@ -1,12 +1,32 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = []
 
 // Snow new quote
 const newQuote = () => {
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // check if author field is blank and replace it with unknown
+  quote.author 
+  ? authorText.textContent = quote.author 
+  : authorText.textContent = 'unknown'
+  //check quote length to determine styling
+  quote.text.length > 120 
+    ? quoteText.classList.add('long-quote') 
+    : quoteText.classList.remove('long-quote');
+
+  quoteText.textContent = quote.text;
 }
 
+// Tweet quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
 
 // Get quotes from api
 const getQuotes = async () => {
@@ -19,6 +39,10 @@ const getQuotes = async () => {
     //handle error here
   }
 }
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
