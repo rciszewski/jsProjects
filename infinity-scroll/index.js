2341bc8bb2ba9ba26e20915cb6960d18c0c1542f
Ron <rc72095@icloud.com>
2023-09-02 12:48:49 -0400

    add event listeners for scrolling and load status to load more images when users scrolls down

--- infinity-scroll/index.js
+++ infinity-scroll/index.js
@@ -1,7 +1,19 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+// check if all images were loaded
+const imageLoaded = () => {
+  console.log('image loaded')
+  imagesLoaded++;
+  if(imagesLoaded === totalImages) ready = true;
+  console.log('ready = ', ready)
+}
+
 
 // Helper function to set attributes on DOM elements
 const setAttributes = (element, attributes) => {
@@ -12,6 +24,8 @@ const setAttributes = (element, attributes) => {
 
 // Create elements for links & photos, Add to DOM
 const displayPhotos = () => {
+  totalImages = photosArray.length;
+  console.log('total images: ', totalImages)
   // Run function for each object in photosArray
   photosArray.forEach(photo => {
     // Create <a> to link to unsplash
@@ -27,6 +41,8 @@ const displayPhotos = () => {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Eventer Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded)
     // put <img> inside <a>, then put both inside imageContainer element
     anchor.appendChild(img);
     imageContainer.appendChild(anchor);
@@ -35,7 +51,7 @@ const displayPhotos = () => {
 
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'Ggay3N2XL7G1axR3DLciEFxzO2khQpkvtri1A98N0q0';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -49,6 +65,14 @@ const getPhotos = async () => {
     console.log('error: ', error)
   }
 }
+
+// check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+    getPhotos()
+    console.log('load more');
+  }
+})
 
 // onLoad
 // getPhotos();
