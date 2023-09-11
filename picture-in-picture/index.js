b0e0c09724cd884bf10835d7ced726367dd23cc2
Ron <rc72095@icloud.com>
2023-09-11 13:35:57 -0400

    add pictureInpicture functionality

--- picture-in-picture/index.js
+++ picture-in-picture/index.js
@@ -1,1 +1,30 @@
-console.log(`testing... it's working!`);
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    console.log('selectMediaStream function error: ', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // disable button
+  button.disabled = true;
+  
+  // start picture in picture
+  await videoElement.requestPictureInPicture();
+  // reset button
+  button.disabled = false;
+});
+
+// On load
+ selectMediaStream();
