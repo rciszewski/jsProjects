a76b603b5eb16511993b8e003351cb7d02feb45d
Ron <rc72095@icloud.com>
2024-07-06 11:47:14 -0400

    load song function on dom mount

--- music-player/script.js
+++ music-player/script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("img");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
 const music = document.querySelector("audio");
 const prevBtn = document.querySelector("#prev");
 const playBtn = document.querySelector("#play");
 const nextBtn = document.querySelector("#next");
+
+//Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 
 function swapPausePlayIcons(isPlaying) {
   if (isPlaying) {
@@ -31,7 +58,58 @@ function pauseSong() {
   music.pause();
 }
 
+function incrementSongSelection() {
+  const currentSongIdx = songs.findIndex((song) => {
+    return song.displayName === title.textContent;
+  });
+  if (currentSongIdx === -1) return;
+  if (currentSongIdx === songs.length - 1) {
+    image.src = `img/${songs[0].name}.jpg`;
+    title.textContent = songs[0].displayName;
+    artist.textContent = songs[0].artist;
+    music.src = `music/${songs[0].name}.mp3`;
+    if (isPlaying) playSong();
+  } else {
+    image.src = `img/${songs[currentSongIdx + 1].name}.jpg`;
+    title.textContent = songs[currentSongIdx + 1].displayName;
+    artist.textContent = songs[currentSongIdx + 1].artist;
+    music.src = `music/${songs[currentSongIdx + 1].name}.mp3`;
+    if (isPlaying) playSong();
+  }
+}
+function decrementSongSelection() {
+  const currentSongIdx = songs.findIndex((song) => {
+    return song.displayName === title.textContent;
+  });
+  if (currentSongIdx === -1) return;
+  if (currentSongIdx === 0) {
+    image.src = `img/${songs[songs.length - 1].name}.jpg`;
+    title.textContent = songs[songs.length - 1].displayName;
+    artist.textContent = songs[songs.length - 1].artist;
+    music.src = `music/${songs[songs.length - 1].name}.mp3`;
+    if (isPlaying) playSong();
+  } else {
+    image.src = `img/${songs[currentSongIdx - 1].name}.jpg`;
+    title.textContent = songs[currentSongIdx - 1].displayName;
+    artist.textContent = songs[currentSongIdx - 1].artist;
+    music.src = `music/${songs[currentSongIdx - 1].name}.mp3`;
+    if (isPlaying) playSong();
+  }
+}
 // Play or pause event listener
 playBtn.addEventListener("click", () => {
   isPlaying ? pauseSong() : playSong();
 });
+nextBtn.addEventListener("click", incrementSongSelection);
+prevBtn.addEventListener("click", decrementSongSelection);
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+//On Load - select first song
+loadSong(songs[3]);
