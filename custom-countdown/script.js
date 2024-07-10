2d2b19d4a460ca98cfeaabc6cc934ecc4613d299
Ron <rc72095@icloud.com>
2024-07-10 16:16:06 -0400

    add complete countdown and reset

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -1,14 +1,20 @@
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
+
 const countDownTitle = document.getElementById("countdown-title");
 const countDownContainer = document.getElementById("countdown");
 const countDownButtonReset = document.getElementById("countdown-button");
 const titleInput = document.getElementById("title");
 const timeElements = document.querySelectorAll("span");
 
+const completeEl = document.getElementById("complete");
+const completeElInfo = document.getElementById("complete-info");
+const completeBtn = document.getElementById("complete-btn");
+
 let countdownValue = Date;
 let countdownActive;
+let countdownTitle = "";
 
 const second = 1000;
 const minute = second * 60;
@@ -31,37 +37,46 @@ const updateDOM = (newDate) => {
     const hourCountdown = Math.floor((timeDifference % day) / hour);
     const minuteCountdown = Math.floor((timeDifference % hour) / minute);
     const secondCountdown = Math.floor((timeDifference % minute) / second);
-
-    timeElements.forEach((span, idx) => {
-      if (idx === 0) {
-        span.textContent = dayCountdown;
-      }
-      if (idx === 1) {
-        span.textContent = hourCountdown;
-      }
-      if (idx === 2) {
-        span.textContent = minuteCountdown;
-      }
-      if (idx === 3) {
-        span.textContent = secondCountdown;
-      }
-    });
+    console.log("time difference", timeDifference);
+    if (timeDifference < 0) {
+      countDownContainer.setAttribute("hidden", true);
+      completeEl.removeAttribute("hidden");
+      clearInterval(countdownActive);
+      completeElInfo.textContent = `${countdownTitle} finished on ${newDate}`;
+    } else {
+      timeElements.forEach((span, idx) => {
+        if (idx === 0) {
+          span.textContent = dayCountdown;
+        }
+        if (idx === 1) {
+          span.textContent = hourCountdown;
+        }
+        if (idx === 2) {
+          span.textContent = minuteCountdown;
+        }
+        if (idx === 3) {
+          span.textContent = secondCountdown;
+        }
+        completeElInfo.hidden = true;
+        countDownContainer.hidden = false;
+      });
+    }
   }, second);
 };
 
 const updateCountDown = (e) => {
   e.preventDefault();
-  const newCountDownTitle = e.srcElement[0].value;
+  countdownTitle = e.srcElement[0].value;
   const newDate = e.srcElement[1].value;
 
-  if (!newCountDownTitle) {
+  if (!countdownTitle) {
     alert("Please create a title for the countdown.");
     return;
   } else if (!newDate) {
     alert("Please select a date for the countdown.");
     return;
   } else {
-    countDownTitle.textContent = newCountDownTitle;
+    countDownTitle.textContent = countdownTitle;
     countDownContainer.removeAttribute("hidden");
     inputContainer.setAttribute("hidden", true);
     titleInput.value = "";
@@ -75,7 +90,9 @@ const resetCountDown = (e) => {
   countDownTitle.textContent = "";
   window.clearInterval(countdownActive);
   dateEl.value = "";
+  completeEl.hidden = true;
 };
 
 countdownForm.addEventListener("submit", updateCountDown);
 countDownButtonReset.addEventListener("click", resetCountDown);
+completeBtn.addEventListener("click", resetCountDown);
