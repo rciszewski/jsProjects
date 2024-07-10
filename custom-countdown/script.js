b72813062b6ee6b41031b67cf9b98b70135a83cc
Ron <rc72095@icloud.com>
2024-07-10 11:16:01 -0400

    add updateDom function to calculate time difference and populate ui

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -1,16 +1,68 @@
-const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('countdownForm');
-const dateEl = document.getElementById('date-picker')
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateEl = document.getElementById("date-picker");
+const countDownTitle = document.getElementById("countdown-title");
+const countDownContainer = document.getElementById("countdown");
+const countDownButtonReset = document.getElementById("countdown-button");
+const titleInput = document.getElementById("title");
+const timeElements = document.querySelectorAll("span");
+
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // set Date input min with today's date
-const today = new Date().toISOString().split('T')[0];
-dateEl.setAttribute('min', today)
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
 
-console.log(today)
+// Population countdown / Complete UI
+const updateDOM = (newDate) => {
+  countdownValue = new Date(newDate).getTime();
+  const today = new Date().getTime();
 
-const datePickerFunction = (e) => {
-  console.log(e.target.value)
-} 
+  const timeDifference = countdownValue - today;
 
-dateEl.addEventListener('change', datePickerFunction)
+  const dayCountdown = Math.floor(timeDifference / day);
+  const hourCountdown = Math.floor((timeDifference % day) / hour);
+  const minuteCountdown = Math.floor((timeDifference % hour) / minute);
+  const secondCountdown = Math.floor((timeDifference % minute) / second);
 
+  timeElements.forEach((span, idx) => {
+    if (idx === 0) {
+      span.textContent = dayCountdown;
+    }
+    if (idx === 1) {
+      span.textContent = hourCountdown;
+    }
+    if (idx === 2) {
+      span.textContent = minuteCountdown;
+    }
+    if (idx === 3) {
+      span.textContent = secondCountdown;
+    }
+  });
+};
+
+const updateCountDown = (e) => {
+  e.preventDefault();
+  const newCountDownTitle = e.srcElement[0].value;
+  const newDate = e.srcElement[1].value;
+  countDownTitle.textContent = newCountDownTitle;
+  countDownContainer.removeAttribute("hidden");
+  inputContainer.setAttribute("hidden", true);
+  titleInput.value = "";
+  updateDOM(newDate);
+};
+
+const resetCountDown = (e) => {
+  console.log(titleInput);
+  inputContainer.removeAttribute("hidden");
+  countDownContainer.setAttribute("hidden", true);
+  countDownTitle.textContent = "";
+};
+
+countdownForm.addEventListener("submit", updateCountDown);
+countDownButtonReset.addEventListener("click", resetCountDown);
