590ecca3dd2507905273604c126823cae748880f
Ron <rc72095@icloud.com>
2024-07-20 13:43:09 -0400

    add event listener to close modal when clicked outside of modal pop up and add form validation

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -10,23 +10,55 @@ const saveBookmarkBtn = document.querySelector("button");
 //show modal, focus input
 const showModal = () => {
   modal.classList.add("show-modal");
-  websiteNameEl.focus()
+  websiteNameEl.focus();
 };
 const closeModal = (e) => {
-  console.log(e)
-  // modal.classList.remove("show-modal");
+  e.target === modal || e.target === modalClose
+    ? modal.classList.remove("show-modal")
+    : false;
 };
 
-const saveBookmark = (e) => {
-  e.preventDefault();
-  console.log(websiteNameEl.value);
-  console.log(websiteUrlEl.value);
-  console.log("bookmark saved");
-};
-
-
-
+//Modal event listeners
 modalShow.addEventListener("click", showModal);
-modalClose.addEventListener("click", closeModal);
-saveBookmarkBtn.addEventListener("click", saveBookmark);
-window.addEventListener('click', closeModal)
+window.addEventListener("click", closeModal);
+
+//validate form
+function validate(nameValue, urlValue) {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+  const regex = new RegExp(expression);
+  console.log(!nameValue);
+  console.log(!urlValue);
+  console.log(urlValue)
+
+  if (!nameValue || !urlValue) {
+    alert("please submit values for both fields");
+    return false;
+  }
+
+  if (urlValue.match(regex)) {
+    alert("matches");
+  } else {
+    alert("please provide a valid web address");
+    return false;
+  }
+  return true;
+}
+
+//Handle data from form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue = `https://${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Event listener
+bookmarkForm.addEventListener("submit", storeBookmark);
